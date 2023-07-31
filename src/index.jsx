8788639f4073771d8fa28e2e3bb85f7bf54f5c7e
LLL <905903576@qq.com>
2023-07-31 14:56:42 +0800

    把 sass 预编译 改为 less /新增 cross-env 修改环境变量的方式来修改端口号

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,11 @@
+/* 多 ES6 内置 API 做兼容处理 */
+
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import '@/index.less';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    {/* <App /> */}
     <div>LLL_study</div>
-  </React.StrictMode>
 );
